5ba64572e997b7d313dc4048554cf2bb60e41723
Raul Ribeiro <raulribeiro@192.168.1.4>
2022-09-16 17:27:20 -0300

    abstraction

--- src/bytebank/herdado/conta/Conta.java
+++ src/bytebank/herdado/conta/Conta.java
@@ -1,7 +1,7 @@
 package bytebank.herdado.conta;
 
-public class Conta {
-	private double saldo;
+public abstract class Conta {
+	protected double saldo;
     private int agencia;
     private int numero;
 	Cliente cliente;
@@ -42,9 +42,7 @@ public class Conta {
 		return saldo;
 	}
 
-	public void deposita(double valor) {
-    	this.saldo += valor;
-    }
+	public abstract void deposita(double valor);
     
     public boolean saca(double valor) {
     	if (saldo >= valor) {
